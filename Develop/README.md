742a4d8fea6b350234107d6d4c2781f62d1d80a4
Jonathan Yee <jyeewasabi@gmail.com>
2020-10-24 19:37:35 -0700

    Readme, Comments & ScreenShots upload

--- Develop/README.md
+++ Develop/README.md
@@ -1,2 +1,20 @@
 # Keystone
 Password Generator
+
+Project to code website for Random Password Generation. Website has single button for user's to click. Then a series of prompts will appear for user to provide their desired criteria for their password. Upon receipt of proper inputs for all four prompts, a randomly generated password will appear. 
+
+## Installation
+
+See Keystone/Develop directory to access all necessary files. CSS and Javascript files (script.js & style.css) run adjacent to index.html. 
+>> Please maintain this orientation for INDEX.HTML to operate properly. 
+
+## Website Display
+
+![Screenshot](/Keystone\Screenshots\1stpage.JPG "Application before Input")
+![Screenshot](/Keystone\Screenshots\1stprompt.JPG "Post User Input, first step")
+
+
+## Contributing
+Jonathan Yee, Junior Developer
+Doublem1nt Industries.
+jyee@jonathanyee.com
--- Develop/script.js
+++ Develop/script.js
@@ -8,11 +8,17 @@ var charNum = "0123456789";
 var charSpec = "!@#$%^&*()_+|}{:?";
 var pool = "";
 
-function writePassword() {
+// Upon click of the Generate Button, Write Password begins asking for Password Criteria
+function generatePassword() {
     alert("Lets begin by specifying the criteria desired for your password.");    
     var upperLowerBoth = prompt("[ CRITERIA#1 ]\nUtilize only lower case, only upper case or both ? \n Input 1 for lower case only \n Input 2 for upper case only \n Input 3 to utilize both");
+
+
+    // While loop forces user to provide only valid input (1,2,3), continues to loop until proper input received. 
     while (upperLowerBoth !== "1" && upperLowerBoth !== "2" && upperLowerBoth !== "3") {
         upperLowerBoth = prompt("Please try that again, you may ONLY input 1, 2 or 3.\n Input 1 for lower case only \n Input 2 for upper case only \n Input 3 to utilze both.");
+
+    // Based on user input, script updates pool with associated string of characters
     } if (upperLowerBoth === "1") {
         pool += charLower;
     } else if (upperLowerBoth === "2") {
@@ -21,6 +27,7 @@ function writePassword() {
         pool = pool + (charUpper + charLower);
     }
 
+    // User defines desired password length
     var lengthPass = prompt("[ CRITERIA#2 ]\n Please specify the desired password length.\n Please Input a number between 8 & 128.");
 
     // Data validation for lengthPass to be integers at/or between 8 and 128. Continues to loop until condition is satisfied. 
@@ -28,30 +35,38 @@ function writePassword() {
         lengthPass = prompt("Please try that again !!\n You may ONLY input #s between (or equal to) 8 and 128.");
     }
 
+    // Convert var lengthPass into a integer
     var intLength = parseInt(lengthPass);
+    
+    // User defines if numbers is desired for password
     var numbersInclude = confirm("[ CRITERIA#3 ]\nWould you like to include numbers in your password?");
 
     if (numbersInclude) {
         pool += charNum;
     }
     
+    // User defines if special characters is desired for password
     var speCharInclude = confirm("[ CRITERIA#4 ]\nLastly, would you like to include special characters?");
+    
     if (speCharInclude) {
         pool += charSpec;
     }
 
-    var password = generatePassword();
-
-    function generatePassword(){
-        var password = "";
-        for (var i=0; i < intLength; i++) {
-            password += pool.charAt(Math.floor(Math.random() * pool.length));
-        }
-        var passwordText = document.querySelector("#password");
-        passwordText.value = password;
-
+    // For Loop generates password after string variable pool contains all character's requested by user
+    var password = "";
+    for (var i=0; i < intLength; i++) {
+        password += pool.charAt(Math.floor(Math.random() * pool.length));
     }
+
+    return password;
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function writePassword() {
+    var password = generatePassword();
+    
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
+  }
+  
+  // Event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
